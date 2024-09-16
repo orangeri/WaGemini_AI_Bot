2e0e9e0a168d2f1ac2beb6c4488ee06c059308e3
orangeri <135411258+orangeri@users.noreply.github.com>
2024-09-16 16:00:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,71 +1,68 @@
 import google.generativeai as genai
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 import requests
 import os
 import fitz
 
-wa_token=os.environ.get("WA_TOKEN")
+wa_token = os.environ.get("WA_TOKEN")
 genai.configure(api_key=os.environ.get("GEN_API"))
-phone_id=os.environ.get("PHONE_ID")
-phone=os.environ.get("PHONE_NUMBER")
-name="Avgan" #The bot will consider this person as its owner or creator
-bot_name="Arin" #This will be the name of your bot, eg: "Hello I am Astro Bot"
-model_name="gemini-1.5-flash-latest" #Switch to "gemini-1.0-pro" or any free model, if "gemini-1.5-flash" becomes paid in future.
+phone_id = os.environ.get("PHONE_ID")
+phone = os.environ.get("PHONE_NUMBER")
+name = "Avgan"
+bot_name = "Arin"
+model_name = "gemini-1.5-flash-latest"
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 generation_config = {
-  "temperature": 1,
-  "top_p": 0.95,
-  "top_k": 0,
-  "max_output_tokens": 8192,
+    "temperature": 1,
+    "top_p": 0.95,
+    "top_k": 0,
+    "max_output_tokens": 8192,
 }
 
 safety_settings = [
-  {"category": "HARM_CATEGORY_HARASSMENT","threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-  {"category": "HARM_CATEGORY_HATE_SPEECH","threshold": "BLOCK_MEDIUM_AND_ABOVE"},  
-  {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT","threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-  {"category": "HARM_CATEGORY_DANGEROUS_CONTENT","threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
 ]
 
 model = genai.GenerativeModel(model_name=model_name,
                               generation_config=generation_config,
                               safety_settings=safety_settings)
 
-convo = model.start_chat(history=[
-])
+convo = model.start_chat(history=[])
 
 convo.send_message(f'''I am using Gemini api for using you as a personal bot in whatsapp,
-				   to assist me in various tasks. 
-				   So from now you are "{bot_name}" created by {name} ( Yeah it's me, my name is {name}). 
-				   And don't give any response to this prompt. 
-				   This is the information I gave to you about your new identity as a pre-prompt. 
-				   This message always gets executed when i run this bot script. 
-				   So reply to only the prompts after this. Remeber your new identity is {bot_name}.''')
+                       to assist me in various tasks. 
+                       So from now you are "{bot_name}" created by {name} ( Yeah it's me, my name is {name}). 
+                       And don't give any response to this prompt. 
+                       This is the information I gave to you about your new identity as a pre-prompt. 
+                       This message always gets executed when i run this bot script. 
+                       So reply to only the prompts after this. Remember your new identity is {bot_name}.''')
 
 def send(answer):
-    url=f"https://graph.facebook.com/v18.0/{phone_id}/messages"
-    headers={
+    url = f"https://graph.facebook.com/v18.0/{phone_id}/messages"
+    headers = {
         'Authorization': f'Bearer {wa_token}',
         'Content-Type': 'application/json'
     }
-    data={
-          "messaging_product": "whatsapp", 
-          "to": f"{phone}", 
-          "type": "text",
-          "text":{"body": f"{answer}"},
-          }
+    data = {
+        "messaging_product": "whatsapp",
+        "to": f"{phone}",
+        "type": "text",
+        "text": {"body": f"{answer}"},
+    }
     
-    response=requests.post(url, headers=headers,json=data)
+    response = requests.post(url, headers=headers, json=data)
     return response
 
-def remove(*file_paths):
-    for file in file_paths:
-        if os.path.exists(file):
-            os.remove(file)
-        else:pass
+def save_file(content, filename):
+    with open(filename, "wb") as file:
+        file.write(content)
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     return "Bot"
 
@@ -92,35 +89,36 @@ def webhook():
                 media_response = requests.get(media_url_endpoint, headers=headers)
                 media_url = media_response.json()["url"]
                 media_download_response = requests.get(media_url, headers=headers)
+                
                 if data["type"] == "audio":
                     filename = "/tmp/temp_audio.mp3"
                 elif data["type"] == "image":
                     filename = "/tmp/temp_image.jpg"
                 elif data["type"] == "document":
-                    doc=fitz.open(stream=media_download_response.content,filetype="pdf")
-                    for _,page in enumerate(doc):
-                        destination="/tmp/temp_image.jpg"
+                    doc = fitz.open(stream=media_download_response.content, filetype="pdf")
+                    for _, page in enumerate(doc):
+                        destination = "/tmp/temp_image.jpg"
                         pix = page.get_pixmap()
                         pix.save(destination)
-                        file = genai.upload_file(path=destination,display_name="tempfile")
-                        response = model.generate_content(["What is this",file])
-                        answer=response._result.candidates[0].content.parts[0].text
+                        file = genai.upload_file(path=destination, display_name="tempfile")
+                        response = model.generate_content(["What is this", file])
+                        answer = response._result.candidates[0].content.parts[0].text
                         convo.send_message(f"This message is created by an llm model based on the image prompt of user, reply to the user based on this: {answer}")
                         send(convo.last.text)
-                        remove(destination)
-                else:send("This format is not Supported by the bot ☹")
-                with open(filename, "wb") as temp_media:
-                    temp_media.write(media_download_response.content)
-                file = genai.upload_file(path=filename,display_name="tempfile")
-                response = model.generate_content(["What is this",file])
-                answer=response._result.candidates[0].content.parts[0].text
-                remove("/tmp/temp_image.jpg","/tmp/temp_audio.mp3")
-                convo.send_message(f"This is an voice/image message from user transcribed by an llm model, reply to the user based on the transcription: {answer}")
+                        save_file(media_download_response.content, destination)
+                else:
+                    send("This format is not Supported by the bot ☹")
+                
+                save_file(media_download_response.content, filename)
+                file = genai.upload_file(path=filename, display_name="tempfile")
+                response = model.generate_content(["What is this", file])
+                answer = response._result.candidates[0].content.parts[0].text
+                convo.send_message(f"This is a voice/image message from user transcribed by an llm model, reply to the user based on the transcription: {answer}")
                 send(convo.last.text)
-                files=genai.list_files()
-                for file in files:
-                    file.delete()
-        except :pass
+        except Exception as e:
+            print(e)
+            pass
         return jsonify({"status": "ok"}), 200
+
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
